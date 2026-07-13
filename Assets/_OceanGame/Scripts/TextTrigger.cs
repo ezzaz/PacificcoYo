b8f0a8004b43711092d3cb821c8dfd0e9eefb6eb
ezzaz <renato12345pinochet@gmail.com>
2026-07-13 10:09:43 -0400

    23

---
+++ Assets/_OceanGame/Scripts/TextTrigger.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TextTrigger : MonoBehaviour
+{
+    [Header("Text Content")]
+    [TextArea(2, 5)] public string textMessage = "Funciona...";
+
+    [Header("Text Configuration")]
+    public bool followCamera = false;
+    public Transform textAnchor; 
+    public Vector3 textSpawnOffset = new Vector3(0f, 2.5f, 0f); 
+    public float displayDuration = 4.5f;
+    public float typingSpeed = 0.04f;
+
+    [Header("Trigger Settings")]
+    public bool triggerOnlyOnce = true;
+    
+    private bool hasTriggered = false;
+
+    private void Awake()
+    {
+        BoxCollider bc = GetComponent<BoxCollider>();
+        bc.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered && triggerOnlyOnce) return;
+
+        if (other.CompareTag("Player"))
+        {
+            SpawnFloatingText();
+            hasTriggered = true;
+        }
+    }
+
+    public void SpawnFloatingText()
+    {
+        GameObject textGo = new GameObject("Text3D");
+        
+        if (followCamera)
+        {
+            textGo.transform.position = transform.position; 
+        }
+        else
+        {
+            if (textAnchor != null)
+            {
+                textGo.transform.position = textAnchor.position;
+                textGo.transform.rotation = textAnchor.rotation;
+            }
+            else
+            {
+                textGo.transform.position = transform.position + textSpawnOffset;
+            }
+        }
+
+        FloatingText3D ft = textGo.AddComponent<FloatingText3D>();
+        ft.textToShow = textMessage;
+        ft.followCamera = followCamera;
+        ft.displayDuration = displayDuration;
+        ft.typingSpeed = typingSpeed;
+        
+       
+    }
+}
